2cb5c9e75a281b717f29b39c3ea2ba8118964dc7
OkIbrahim <155417872+OkIbrahim@users.noreply.github.com>
2025-11-25 10:28:16 +0100

    Implement section reveal on scroll
    Added functionality to reveal sections on scroll.

--- script.js
+++ script.js
@@ -60,3 +60,25 @@ navLinkEl.forEach((link) =>
     headerEl.classList.remove("nav-open");
   })
 );
+
+// Reveal sections
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove("section--hidden");
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
